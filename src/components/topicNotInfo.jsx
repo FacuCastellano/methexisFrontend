1552e953b79537b53a3821e869b4a2a2af78c23a
Facundo Castellano <castellanofacundo@gmail.com>
2023-06-02 17:43:38 -0300

    se creo el componente topicNotInfo

--- src/components/topicNotInfo.jsx
+++ src/components/topicNotInfo.jsx
@@ -1,14 +1,10 @@
-export function TopicNotInfo(props) {
+export function TopicNotInfo() {
   
   return (
     <div className="topicNotInfo">
-      <TopicVideo videoId={videoId} />
-      <TopicDetails
-        rank={rank}
-        totalPoints={totalPoints}
-        totalVotes={totalVotes}
-        comments={comments}
-      />
+      <p>This topic not has info</p>
+      <p>to watch and example got to</p>
+      <p>"Nivel 3" "Matematica/materia 3" "unit3" "tema3"</p>
     </div>
   );
 }
